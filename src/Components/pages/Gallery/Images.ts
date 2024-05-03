645fd97056e1789c5a3cee3e8d26da7be3d2a2d5
Vicky Manavadariya <vicky@weybee.in>
2024-05-03 13:14:11 +0530

    gallery fix

--- src/Components/pages/Gallery/Images.ts
+++ src/Components/pages/Gallery/Images.ts
@@ -1,5 +1,18 @@
 import { Image } from "react-grid-gallery";
-import img from '../../../assets/Gallery/IMG_3354.webp'
+import IMG_3354 from '../../../assets/Gallery/IMG_3354.webp';
+import IMG_3355 from '../../../assets/Gallery/IMG_3355.webp';
+import IMG_3356 from '../../../assets/Gallery/IMG_3356.webp';
+import IMG_3357 from '../../../assets/Gallery/IMG_3357.webp';
+import IMG_3358 from '../../../assets/Gallery/IMG_3358.webp';
+import IMG_3359 from '../../../assets/Gallery/IMG_3359.webp';
+import IMG_3360 from '../../../assets/Gallery/IMG_3360.webp';
+import IMG_3361 from '../../../assets/Gallery/IMG_3361.webp';
+import IMG_3362 from '../../../assets/Gallery/IMG_3362.webp';
+import IMG_3363 from '../../../assets/Gallery/IMG_3363.webp';
+import IMG_3364 from '../../../assets/Gallery/IMG_3364.webp';
+import IMG_3365 from '../../../assets/Gallery/IMG_3365.webp';
+import IMG_3366 from '../../../assets/Gallery/IMG_3366.webp';
+import IMG_3367 from '../../../assets/Gallery/IMG_3367.webp';
 
 export interface CustomImage extends Image {
   original: string;
@@ -26,85 +39,85 @@ export const images: CustomImage[] = [
     //     height: 4032
     // },
     {
-        src: "src/assets/Gallery/IMG_3354.webp",
+        src: IMG_3354,
         original: "src/assets/Gallery/IMG_3354.webp",
         width: 3024,
         height: 4032
     },
     {
-        src: "src/assets/Gallery/IMG_3355.webp",
+        src: IMG_3355,
         original: "src/assets/Gallery/IMG_3355.webp",
         width: 3024,
         height: 4032
     },
     {
-        src: "src/assets/Gallery/IMG_3356.webp",
+        src: IMG_3356,
         original: "src/assets/Gallery/IMG_3356.webp",
         width: 4032,
         height: 3024
     },
     {
-        src: "src/assets/Gallery/IMG_3357.webp",
+        src: IMG_3357,
         original: "src/assets/Gallery/IMG_3357.webp",
         width: 3024,
         height: 4032
     },
     {
-        src: "src/assets/Gallery/IMG_3358.webp",
+        src: IMG_3358,
         original: "src/assets/Gallery/IMG_3358.webp",
         width: 3024,
         height: 4032
     },
     {
-        src: "src/assets/Gallery/IMG_3359.webp",
+        src: IMG_3359,
         original: "src/assets/Gallery/IMG_3359.webp",
         width: 3024,
         height: 4032
     },
     {
-        src: "src/assets/Gallery/IMG_3360.webp",
+        src: IMG_3360,
         original: "src/assets/Gallery/IMG_3360.webp",
         width: 3024,
         height: 4032
     },
     {
-        src: "src/assets/Gallery/IMG_3361.webp",
+        src: IMG_3361,
         original: "src/assets/Gallery/IMG_3361.webp",
         width: 4032,
         height: 3024
     },
     {
-        src: "src/assets/Gallery/IMG_3362.webp",
+        src: IMG_3362,
         original: "src/assets/Gallery/IMG_3362.webp",
         width: 3024,
         height: 4032
     },
     {
-        src: "src/assets/Gallery/IMG_3363.webp",
+        src: IMG_3363,
         original: "src/assets/Gallery/IMG_3363.webp",
         width: 4032,
         height: 3024
     },
     {
-        src: "src/assets/Gallery/IMG_3364.webp",
+        src: IMG_3364,
         original: "src/assets/Gallery/IMG_3364.webp",
         width: 4032,
         height: 3024,
     },
     {
-        src: "src/assets/Gallery/IMG_3365.webp",
+        src: IMG_3365,
         original: "src/assets/Gallery/IMG_3365.webp",
         width: 4032,
         height: 3024
     },
     {
-        src: "src/assets/Gallery/IMG_3366.webp",
+        src: IMG_3366,
         original: "src/assets/Gallery/IMG_3366.webp",
         width: 4032,
         height: 3024
     },
     {
-        src: "src/assets/Gallery/IMG_3367.webp",
+        src: IMG_3367,
         original: "src/assets/Gallery/IMG_3367.webp",
         width: 4032,
         height: 3024
