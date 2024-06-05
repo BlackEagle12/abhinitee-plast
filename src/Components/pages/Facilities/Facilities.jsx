b982c8f8735212549b1966a0d6f72261ebd6971d
Vicky Manavadariya <vicky@weybee.in>
2024-06-05 19:13:32 +0530

    sdfasdfsd

--- src/Components/pages/Facilities/Facilities.jsx
+++ src/Components/pages/Facilities/Facilities.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import SectionHeading from '../../Common/SectionHeading';
 
 function Facilities(props) {
     return (
-        <div>
-            Facilities
-        </div>
+        <>
+            <div className="section-heading-container">
+				<SectionHeading
+					backgroundText="Plast"
+					headingText1="Tool"
+					headingText2="room"
+				/>
+			</div>
+        </>
     );
 }
 
